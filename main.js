b0255bd84ded7ff364a3fcd79bec28b29ee66ce9
Revants26 <85337734+Revants26@users.noreply.github.com>
2022-01-03 15:42:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,73 @@
-menu_list_array = ["Chicken Pizza","Paneer Pizza","Pepperoni Pizza", "Margherita Pizza"];
+// Initializing the array
+var names_of_people = [];
     
-function getmenu(){
-    var htmldata;
-    htmldata="<ol class='menulist'>"
-    menu_list_array.sort();
-    for(var i=0;i<menu_list_array.length;i++){
-        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
-    }
-     htmldata=htmldata+"</ol>"
-     document.getElementById("display_menu").innerHTML = htmldata;
-}
+function submit()
+{
+	// Storing the value of the input in a variable
+    var GuestName = document.getElementById("name1").value;
 
-function add_item(){
-    var htmldata;
-    var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-    menu_list_array.sort();
-    htmldata="<section class='cards'>"
-    for(var i=0;i<menu_list_array.length;i++){
+	// Adding items to the array defined in the beggining
+	names_of_people.push(GuestName);
+	console.log(GuestName);    
+    console.log(names_of_people);
 
-        htmldata=htmldata+'<div class="card">' +'<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
-    }
-      htmldata=htmldata+"</section>"
-      document.getElementById("display_addedmenu").innerHTML = htmldata;
-      document.getElementById("add_item").value = "";
+	// Getting the length of the array
+    var lenght_of_name = names_of_people.length;
+    console.log(lenght_of_name);
+
+	// Displaying the array
+	document.getElementById("display_name").innerHTML=names_of_people.toString();
+   }
+
+
+
+function show()
+{
+	// Seperate the items by creating a new line
+	var i= names_of_people.join("<br>");
+	console.log(names_of_people);
+
+	// Displaying the array
+	document.getElementById("p1").innerHTML=i.toString();
+
+	// Displaying the sort button
+	document.getElementById("sort_button").style.display="block";
+	}
+
+
+function sorting()
+	{
+		// Sorting the array in alphabetical order
+		names_of_people.sort();
+
+		// Seperating the array items by creating a new line
+		var i= names_of_people.join("<br>");
+		console.log(names_of_people);		
+
+		// Displaying the sorted array
+		document.getElementById("sorted").innerHTML=i.toString();
+		}
+
+
+function searching()
+{
+	// Storing the value of the search input in a variable
+	var s= document.getElementById("s1").value;
+
+	// Creating variables that will be used in the for loop
+	var found=0;
+	var j;
+
+	// Starting the for loop
+	for(j=0; j<names_of_people.length; j++)
+		{
+			// If the item contains what the person searches, increase the variable by 1 until the loop reaches the length of the array
+			if(s==names_of_people[j]){
+				found=found+1;
+			}
+		}
+
+	// Display the number of items found
+	document.getElementById("p2").innerHTML="name found "+found+" time/s";
+	console.log("found name "+found+" time/s");
 }
