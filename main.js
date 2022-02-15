849cdf57a7af62b975203f723fcc4dffd423eabc
Revants26 <85337734+Revants26@users.noreply.github.com>
2022-02-15 19:07:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,67 @@
-menu_list_array = ["Chicken Pizza","Paneer Pizza","Pepperoni Pizza", "Margherita Pizza"];
-    
-function getmenu(){
-    var htmldata;
-    htmldata="<ol class='menulist'>"
-    menu_list_array.sort();
-    for(var i=0;i<menu_list_array.length;i++){
-        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
-    }
-     htmldata=htmldata+"</ol>"
-     document.getElementById("display_menu").innerHTML = htmldata;
+canvas = document.getElementById('myCanvas');
+
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
 
-function add_item(){
-    var htmldata;
-    var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-    menu_list_array.sort();
-    htmldata="<section class='cards'>"
-    for(var i=0;i<menu_list_array.length;i++){
+function uploadimg() {
 
-        htmldata=htmldata+'<div class="card">' +'<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
-    }
-      htmldata=htmldata+"</section>"
-      document.getElementById("display_addedmenu").innerHTML = htmldata;
-      document.getElementById("add_item").value = "";
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
+
+window.addEventListener("keydown", my_keydown );
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+                  aplhabetkey();
+				  document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+				  console.log("alphabet key");
+		}
+
+		
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png";
+	add();
+
+}
+function numberkey()
+{
+	
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
